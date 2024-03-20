37ce80bf619fd631d85c2fe5679a30b2f0927def
Phuong <phuwn29@gmail.com>
2024-03-20 20:04:04 +0700

    feat: Update ListKeywordsView

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import axios from 'axios';
 import { ref } from 'vue';
-import { userInfoStore } from "../store/userInfoStore";
-defineProps<{ msg?: string, uploadFile?: (any) => void }>()
-const { accessToken, update } = userInfoStore()
+import { keywordStore } from "../store/keywordStore";
+defineProps<{ msg?: string }>()
 const fileInput = ref<HTMLInputElement | null>(null)
+const { uploadKeywordsFile } = keywordStore()
+
 </script>
 
 <template>
 	<div>
 		<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">{{ msg }}</label>
-		<input id="file_input" type="file" ref="fileInput" style="display:none" @change="uploadFile">
+		<input id="file_input" type="file" ref="fileInput" style="display:none" @change="uploadKeywordsFile">
 		<button type="button"
 			class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
 			@click="fileInput?.click()">Upload File</button>
