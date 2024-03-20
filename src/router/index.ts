a558f0928e2fb40490b69e00ba2a1f340850478f
Phuong <phuwn29@gmail.com>
2024-03-20 20:06:04 +0700

    feat: Keyword Details View

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import ListKeywordsView from '../views/ListKeywordsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import ListKeywordsView from "../views/ListKeywordsView.vue";
+import DetailView from "../views/DetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
     {
-      path: '/list-keywords',
-      name: 'list-keywords',
+      path: "/list-keywords",
+      name: "list-keywords",
       component: ListKeywordsView,
-    }
-  ]
-})
+    },
+    {
+      path: "/keywords/:name",
+      name: "keywords-detail",
+      component: DetailView,
+      props: true,
+    },
+  ],
+});
 
-router.beforeEach((to, from)=>{
-  const userInfoJson = localStorage.getItem('user_info')
-  const userInfo = JSON.parse(`${userInfoJson}`)
-  if (!userInfo.loggedIn && (to.path != '/' && to.path != '/login')) {
-    return {path: '/'}
+router.beforeEach((to, from) => {
+  const userInfoJson = localStorage.getItem("user_info");
+  const userInfo = JSON.parse(`${userInfoJson}`);
+  if (!userInfo.loggedIn && to.path != "/" && to.path != "/login") {
+    return { path: "/" };
   }
-})
+});
 
-export default router
+export default router;
